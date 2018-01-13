b7dbe539abc6424456e674792ace4c2c4636c327
yangfeng <yangfeng@yangfeng.local>
2018-01-13 23:12:11 +0800

    comment activity and add poto networking

--- PagingWithNetworkSample/app/src/main/java/com/android/example/paging/pagingwithnetwork/reddit/ui/RedditActivity.kt
+++ PagingWithNetworkSample/app/src/main/java/com/android/example/paging/pagingwithnetwork/reddit/ui/RedditActivity.kt
@@ -17,8 +17,6 @@
 package com.android.example.paging.pagingwithnetwork.reddit.ui
 
 import android.arch.lifecycle.Observer
-import android.arch.lifecycle.ViewModel
-import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.arch.paging.PagedList
 import android.content.Context
@@ -28,7 +26,6 @@ import android.support.v7.app.AppCompatActivity
 import android.view.KeyEvent
 import android.view.inputmethod.EditorInfo
 import com.android.example.paging.pagingwithnetwork.R
-import com.android.example.paging.pagingwithnetwork.reddit.ServiceLocator
 import com.android.example.paging.pagingwithnetwork.base.repository.NetworkState
 import com.android.example.paging.pagingwithnetwork.reddit.repository.RedditPostRepository
 import com.android.example.paging.pagingwithnetwork.reddit.vo.RedditPost
@@ -41,10 +38,13 @@ import kotlinx.android.synthetic.main.activity_reddit.*
  * The intent arguments can be modified to make it use a different repository (see MainActivity).
  */
 class RedditActivity : AppCompatActivity() {
+    // 静态常量和函数
     companion object {
         val KEY_SUBREDDIT = "subreddit"
         val DEFAULT_SUBREDDIT = "androiddev"
         val KEY_REPOSITORY_TYPE = "repository_type"
+
+        // 根据不同的类型参数，创建不同的Intent，把参数会给activity
         fun intentFor(context: Context, type: RedditPostRepository.Type): Intent {
             val intent = Intent(context, RedditActivity::class.java)
             intent.putExtra(KEY_REPOSITORY_TYPE, type.ordinal)
@@ -52,8 +52,11 @@ class RedditActivity : AppCompatActivity() {
         }
     }
 
+    // ViewModel对象，todo: lateinit关键字表示对象变量延后赋值？？？
     private lateinit var model: SubRedditViewModel
 
+    // 创建activity对象时，设置content view, 创建view model, 初始化adapter，下拉刷新和搜索框，
+    // 读取之前保存的搜索关键字或者用默认初始值，然后作为参数调用view model的方法显示关键字的数据
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reddit)
@@ -65,19 +68,18 @@ class RedditActivity : AppCompatActivity() {
         model.showSubreddit(subreddit)
     }
 
+    // 创建view model, 通过实现ViewModelProvide.Factory来控制view model构造时把想要的数据来源（仓库类型）
+    // 对应参数和activity的Context传递给对应于它的Factory类(RedditViewModelFactory, 并由创建view model
+    // 同时把参数传过去
     private fun getViewModel(): SubRedditViewModel {
-        return ViewModelProviders.of(this, object : ViewModelProvider.Factory {
-            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-                val repoTypeParam = intent.getIntExtra(KEY_REPOSITORY_TYPE, 0)
-                val repoType = RedditPostRepository.Type.values()[repoTypeParam]
-                val repo = ServiceLocator.instance(this@RedditActivity)
-                        .getRepository(repoType)
-                @Suppress("UNCHECKED_CAST")
-                return SubRedditViewModel(repo) as T
-            }
-        })[SubRedditViewModel::class.java]
+        val repoTypeParam = intent.getIntExtra(KEY_REPOSITORY_TYPE, 0)
+        return ViewModelProviders.of(this,
+                RedditViewModelFactory(this, repoTypeParam))[SubRedditViewModel::class.java]
     }
 
+    // 初始化图片加载库Glide， 创建页面视图的Adapter（完成后），运行闭包调view model重新取数据(retry)
+    // 观察view model里的post列表的LiveData，当数据变化时把分页列表PagedList设置给adapter
+    // 观察view model里网络状态(networkState)的LiveData, 数据变化时，设置到adapter里
     private fun initAdapter() {
         val glide = Glide.with(this)
         val adapter = PostsAdapter(glide) {
@@ -92,6 +94,7 @@ class RedditActivity : AppCompatActivity() {
         })
     }
 
+    // 初始化下拉刷新，观察刷新状态LiveData，刷新控件刷新，调view model刷新(refresh)一下
     private fun initSwipeToRefresh() {
         model.refreshState.observe(this, Observer {
             swipe_refresh.isRefreshing = it == NetworkState.LOADING
@@ -101,11 +104,13 @@ class RedditActivity : AppCompatActivity() {
         }
     }
 
+    // 保留既出前的搜索关键定，重新创建时
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString(KEY_SUBREDDIT, model.currentSubreddit())
     }
 
+    // 初始化搜索框
     private fun initSearch() {
         input.setOnEditorActionListener({ _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_GO) {
